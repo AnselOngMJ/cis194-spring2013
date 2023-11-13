6fd7fd06eb970b80695cb15d405b369a17bcbcd8
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-11-13 13:28:16 +0000

    Add comments to toDigits & toDigitsRev

--- hw1.hs
+++ hw1.hs
@@ -1,8 +1,11 @@
 {-# OPTIONS_GHC -Wall #-}
 
 -- Exercise 1
+-- Converts a positive Integer to a list of digits
 toDigits :: Integer -> [Integer]
 toDigits = reverse . toDigitsRev
+
+-- Converts a positive Integer to a list of digits in reverse
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = [n `div` 10 ^ x `mod` 10
     | n > 0, let xs = [0..length(show n) - 1], x  <- xs]
