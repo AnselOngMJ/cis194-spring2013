21d104bc3e89dce086fa712d886b7449849c033e
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-11-13 14:04:20 +0000

    Switch variable names

--- hw1.hs
+++ hw1.hs
@@ -7,17 +7,17 @@ toDigits = reverse . toDigitsRev
 
 -- Converts a positive Integer to a list of digits in reverse
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = [n `div` 10 ^ x `mod` 10
-    | n > 0, let xs = [0..length(show n) - 1], x  <- xs]
+toDigitsRev x = [x `div` 10 ^ n `mod` 10
+    | x > 0, let ns = [0..length(show x) - 1], n  <- ns]
 
 -- Exercise 2
 -- Doubles every other digit starting from the right
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther ns = [ns !! (m - x) * isEvenToDigit x
-    | let m = length ns, let xs = [m, m - 1..1], x <- xs]
+doubleEveryOther xs = [xs !! (m - n) * isEvenToDigit n
+    | let m = length xs, let ns = [m, m - 1..1], n <- ns]
 
 -- Returns 2 if digit is even else returns 1
 isEvenToDigit :: Int -> Integer
-isEvenToDigit n
-    | even n    = 2
+isEvenToDigit x
+    | even x    = 2
     | otherwise = 1
