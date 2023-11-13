95cac808b5110ab5f3231d82fd26e48972041f43
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-11-13 13:59:28 +0000

    Complete Homework 1 Exercise 2

--- hw1.hs
+++ hw1.hs
@@ -9,3 +9,15 @@ toDigits = reverse . toDigitsRev
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = [n `div` 10 ^ x `mod` 10
     | n > 0, let xs = [0..length(show n) - 1], x  <- xs]
+
+-- Exercise 2
+-- Doubles every other digit starting from the right
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther ns = [ns !! (m - x) * isEvenToDigit x
+    | let m = length ns, let xs = [m, m - 1..1], x <- xs]
+
+-- Returns 2 if digit is even else returns 1
+isEvenToDigit :: Int -> Integer
+isEvenToDigit n
+    | even n    = 2
+    | otherwise = 1
